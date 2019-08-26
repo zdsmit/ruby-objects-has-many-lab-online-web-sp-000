cc4d220cc988f99d738552ee10ac9bb316aa7b39
Zach  Smith <zdsmith001@gmail.com>
2019-08-26 17:42:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,18 @@
 class Author
   attr_accessor :name
-  @@posts = []
+  @@all = []
 
   def initialize(name)
     @name = name
     @@posts << self
   end
 
-  def posts
+  def self.all
     @@posts
+  end
+
+  def posts
+    Post.all.select {|post|post.author == self}
   end
 
 end
